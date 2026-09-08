ffe84afd7a2a624658bfb0fd2c8514a2e9da3b40
elfaquihiayoub <elfaquihi.ayoub@gmail.com>
2026-09-08 09:17:50 +0100

    adding changeDeValeur algo and chercherValeurMaximal algo

---
+++ sprint 1/ChangeDeValeur.js
@@ -0,0 +1,16 @@
+// algo 1 
+
+function valueSwapping(v1,v2){
+    let v3;
+    v3=v1;
+    v1=v2;
+    v2=v3;
+
+    return `v1 = ${v1} and v2 = ${v2} `
+}
+
+console.log(valueSwapping(10,20));
+// algorithme 2 
+
+
+
--- sprint 1/ChercheValuerMaximal.js
+++ sprint 1/ChercheValuerMaximal.js
@@ -1,19 +1,3 @@
-// algo 1 
-
-function valueSwapping(v1,v2){
-    let v3;
-    v3=v1;
-    v1=v2;
-    v2=v3;
-
-    return `v1 = ${v1} and v2 = ${v2} `
-}
-
-console.log(valueSwapping(10,20));
-// algorithme 2 
-
-
-
 function getMaxValue(){
     let array=[1,10,5]
     let max=0;
