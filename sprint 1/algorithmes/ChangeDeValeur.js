fd5340fecf7c7611541d0ff85f4c2b85ee3ed40b
elfaquihiayoub <elfaquihi.ayoub@gmail.com>
2026-09-08 13:20:50 +0100

    fixing swapoing value algorithm

--- sprint 1/algorithmes/ChangeDeValeur.js
+++ sprint 1/algorithmes/ChangeDeValeur.js
@@ -1,15 +1,11 @@
-// algo 1 
 
-function valueSwapping(v1,v2){
-    let v3;
-    v3=v1;
-    v1=v2;
-    v2=v3;
-
-    return `v1 = ${v1} and v2 = ${v2} `
-}
-
-console.log(valueSwapping(10,20));
+let v1 =10;
+let v2 =20;
+let v3;
+v3=v1;
+v1=v2;
+v2=v3;
+console.log(v1+' et ' + v2);
 
 
 
